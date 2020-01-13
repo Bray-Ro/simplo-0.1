40f683dd4945f7a4273cd87afbeca4a9a9110320
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-01-13 07:52:28 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,27 @@
 # uses python3
+
+import os
 from random import *
+import psutil
 from termcolor import colored
+program = []
 variable_names = []
 variable_values = []
 variables = -1
 value = 0
 print_var = None
+print(colored("**** Skyscript v2 ****", "green"))
+print(psutil.virtual_memory())
+print()
 def new_command():
+    global program
     global print_var
     global variables
     global value
     global variable_values
     global variable_names
-    command = input("skyscript 2 >")
+   
+    command = input("Mac Skyscript V2 >")
     if command[:4] == "ling":
        
         variables += 1
@@ -31,21 +40,34 @@ def new_command():
     if command[:5] == "echo ":
         command = command.replace(command[:4], '')
         print(command)
-    if command[:10] == "echo_var: ":
-        command = command.replace(command[:10], '')
+    if command[:9] == "echo_var ":
+        command = command.replace(command[:9], '')
         command = variable_names.index(command)
         value = variable_values[command]
         print(value)    
-    if command[5] == "add: ":
-        command = command.replace(command[:5, ''])
-        
-        problem = command.split(' ')
-        value1 = problem[0]
-        value2 = problem[2]
-        value1 = int(value1)
-        value2 = int(value2)
-        print(str(problem))
-        command = str(value1 + value2)
-        print(command)
+    if command == "new_program":
+        print(colored("Creating a new program...", "green"))
+        program = []
+        lines = 0
+        running_line = -1
+        line_running = None
+        def run(running_line):
+            
+            running_line += 1
+            if running_line == lines:
+                print("program completed")
+            else:
+                line_running = program[lines]
+                if  program[:5] == "echo ":
+                    command = command.replace(command[:4], '')
+                    print(command)
+        def new_line(lines):
+            
+            lines += 1
+            
+            line = input("> ")
+            program.insert(lines, str(line))
+            new_line(lines)
+        new_line(lines)
     new_command()
 new_command()
