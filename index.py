599e743641e1e96b4d87b3820dde8b6bbac410d8
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-01-29 19:29:41 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,73 +1,68 @@
-# uses python3
 
-import os
-from random import *
-import psutil
-from termcolor import colored
-program = []
-variable_names = []
-variable_values = []
-variables = -1
-value = 0
-print_var = None
-print(colored("**** Skyscript v2 ****", "green"))
-print(psutil.virtual_memory())
-print()
-def new_command():
-    global program
-    global print_var
-    global variables
-    global value
-    global variable_values
-    global variable_names
-   
-    command = input("Mac Skyscript V2 >")
-    if command[:4] == "ling":
-       
-        variables += 1
-        value = command.replace(command[:12], '')
+#!/usr/bin/env python
+# work on ling command next
+# simplo transpiler version 0.1 BETA by Rohan Samra-O'Neill
+import sys
+for arg in sys.argv:
+    with open(arg) as file:
+        goto_repeat = 0
+        goto_on = 0
+        variable_names = []
+        variable_values = []
+        variables = -1
+        value = None
+        prog = file.read()
+        prog = prog.split(';')
         
-        command = command.replace(command[:4], '')
-        command = command.split()
-        var_length = len(command)
-        
-        variable_names.insert(variables, command[0])
-        print(command)
-        print("var names: " + str(variable_names))
-
-        variable_values.insert(variables, value)
-        print(colored("Variable " + variable_names[variables] + " has been made with a value of " + value, "green"))
-    if command[:5] == "echo ":
-        command = command.replace(command[:4], '')
-        print(command)
-    if command[:9] == "echo_var ":
-        command = command.replace(command[:9], '')
-        command = variable_names.index(command)
-        value = variable_values[command]
-        print(value)    
-    if command == "new_program":
-        print(colored("Creating a new program...", "green"))
-        program = []
-        lines = 0
-        running_line = -1
+        running_line = 0
         line_running = None
-        def run(running_line):
+        def new_line():
+            global goto_on
+            global goto_repeat
+            global value
+            global prog
+            global running_line
+            global line_running
+            global variables
+            global variable_values
+            global variable_names
             
-            running_line += 1
-            if running_line == lines:
-                print("program completed")
-            else:
-                line_running = program[lines]
-                if  program[:5] == "echo ":
-                    command = command.replace(command[:4], '')
-                    print(command)
-        def new_line(lines):
-            
-            lines += 1
-            
-            line = input("> ")
-            program.insert(lines, str(line))
-            new_line(lines)
-        new_line(lines)
-    new_command()
-new_command()
+            line_running = prog[running_line]
+            if line_running[:1] == '#':
+                running_line += 1
+                new_line()
+            if  line_running[:5] == 'echo ':
+                
+                line_running = line_running.replace(line_running[:5], '')
+                
+                print(line_running)
+                
+                if running_line == len(prog):
+                    print("program completed")
+                else:
+                    running_line += 1
+                    new_line()
+                    # WORK ON NEXT
+            if line_running[:5] == "goto ":
+                line_running = line_running.replace(line_running[:5], '')
+                
+                line_running = line_running.replace(line_running[2:], '')
+                if goto_repeat == 0:
+                  goto_repeat = line_running[1:]
+                  goto_on = 0
+                goto_on += 1
+                
+                running_line = line_running
+                running_line = int(float(running_line))
+                if goto_on == 300:
+                  print(running_line)
+                else:
+                  
+                  new_line()
+
+                    
+        new_line()
+                    
+
+
+
